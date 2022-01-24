c0cb42942fb15ac4262b44c4f77fdcb7e4266277
cyanide-pixel <54498203+cyanide-pixel@users.noreply.github.com>
2022-01-24 13:33:30 +0530

    Time: 71 ms (58.79%), Space: 48.3 MB (79.46%) - LeetHub

---
+++ 55-jump-game/55-jump-game.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        
+        int n = nums.size();
+        int goodPos = n-1;
+        
+        
+        for(int i=n-2; i>=0; i-- )
+        {
+            if(i+nums[i]>=goodPos)
+            {
+                goodPos = i;
+            }
+        }
+        
+        if(goodPos == 0) return true;
+        else return false;
+            
+        
+    }
+};
