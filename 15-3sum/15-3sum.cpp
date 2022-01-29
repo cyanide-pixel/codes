195a3205dd58e8976ce98a7dbb545861e944afe1
cyanide-pixel <54498203+cyanide-pixel@users.noreply.github.com>
2022-01-29 17:43:45 +0530

    Time: 131 ms (45.91%), Space: 19.9 MB (86.33%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -1,8 +1,9 @@
 class Solution {
 public:
+    
     vector<vector<int>> threeSum(vector<int>& nums) {
         
-        vector<vector<int> > res;
+              vector<vector<int> > res;
         
         sort(nums.begin(), nums.end());
         int sum, n= nums.size();
@@ -36,6 +37,44 @@ public:
             }
         }
         return res;
+        
+        
+        
+//         vector<vector<int> > res;
+        
+//         sort(nums.begin(), nums.end());
+//         int sum, n= nums.size();
+        
+//         for(int i=0; i<n-2 ;i++) {
+//             if(i==0 || nums[i] !=nums[i-1]) {
+//                 int l=i+1;
+//                 int r=n-1;
+                
+//                 while(l<r) {
+//                     sum = nums[i]+nums[l]+nums[r];
+                    
+//                     if(sum==0) {
+//                         res.push_back({nums[i], nums[l], nums[r]});
+                        
+//                         while(l<r and nums[l] == nums[l+1]) {
+//                             l++;
+//                         }
+//                         while(l<r and nums[r] == nums[r-1]) {
+//                             r--;
+//                         }
+//                         l++;
+//                         r--;
+                        
+//                     }
+//                     else if(sum <0) {
+//                         l++;
+                         
+//                     }
+//                     else r--;
+//                 }
+//             }
+//         }
+//         return res;
         
     }
 };
