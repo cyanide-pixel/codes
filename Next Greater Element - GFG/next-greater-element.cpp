2ceb1c4af7c38134a6f29a4b935ade94d9890e50
cyanide-pixel <54498203+cyanide-pixel@users.noreply.github.com>
2022-02-01 19:49:13 +0530

    Added solution - LeetHub

--- Next Greater Element - GFG/next-greater-element.cpp
+++ Next Greater Element - GFG/next-greater-element.cpp
@@ -13,15 +13,15 @@ class Solution
         
         vector <long long> v(n);
         
-        stack <long long> s;
+        stack <long long> st;
         for(int i=n-1; i>=0; i--){
             
            
-            while(!s.empty() and s.top()<arr[i])
-                s.pop();
+            while(!st.empty() and st.top()<arr[i])
+                st.pop();
         
-            v[i] = s.empty() ? -1 : s.top();
-            s.push(arr[i]);
+            v[i] = st.empty() ? -1 : st.top();
+            st.push(arr[i]);
         }
         return v;
     }
