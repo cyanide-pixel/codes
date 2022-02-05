110d6d1cdf2861befe7c7ae81f1ad0ef66814894
cyanide-pixel <54498203+cyanide-pixel@users.noreply.github.com>
2022-02-05 08:47:06 +0530

    Time: 4 ms (84.82%), Space: 7.2 MB (29.33%) - LeetHub

---
+++ 402-remove-k-digits/402-remove-k-digits.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    string removeKdigits(string num, int k) {
+        
+                stack<char> s;
+        s.push(num[0]);
+        int i=1;
+        int len=num.length();
+        while(i<len)
+        {
+            //char top=s.top();
+            if(num[i]>=s.top())
+            {
+                s.push(num[i]);
+            }
+            else
+            {
+                while(k>0 && !s.empty())
+                {
+                    if(num[i]<s.top())
+                    {s.pop();
+                    k--;}
+                    else
+                        break;
+                }
+                s.push(num[i]);
+            }
+            i++;
+        }
+        while(k>0)
+        {
+            s.pop();
+            k--;
+        }
+        string ans="";
+        while(!s.empty())
+        {
+            char a=s.top();
+            ans+=a;
+            s.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        i = 0;
+        while(i< ans.length() && ans[i] == '0'){
+            i++;
+        }
+        if(i == ans.length()){
+            return "0";
+        }
+        return ans.substr(i, len-i);
+    }
+};
