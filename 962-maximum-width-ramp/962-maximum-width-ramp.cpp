56aaaad22bd6c8b178d3db16c5e864a090ea456d
cyanide-pixel <54498203+cyanide-pixel@users.noreply.github.com>
2022-02-05 21:24:04 +0530

    Time: 59 ms (61.12%), Space: 29.4 MB (64.66%) - LeetHub

--- 962-maximum-width-ramp/962-maximum-width-ramp.cpp
+++ 962-maximum-width-ramp/962-maximum-width-ramp.cpp
@@ -1,25 +1,29 @@
 class Solution {
 public:
-    int maxWidthRamp(vector<int>& A) {
+    int maxWidthRamp(vector<int>& nums) {
         
-     int n = A.size();
-     stack<int> st;;
-     for (int i = 0; i < n; i++)
-     {
-      if (st.empty() || A[i] < A[st.top()])
-       {
-         st.push(i);
-       }
-     }
-     int ans = 0;
-     for (int i = n - 1; i >= 0; i--)
-     {
-       while (!st.empty() && A[i] >= A[st.top()])
-       {
-          ans = max(i - st.top(), ans);
-          st.pop();
-       }
-     }
-     return ans;
+        stack<int> st;
+        int n = nums.size();
+        
+        for(int i=0; i<n; i++)
+        {
+            if(st.empty() || nums[i]<nums[st.top()])
+            {
+                st.push(i);
+            }
+        }
+        
+        int res = 0;
+        
+        for(int i = n-1; i>=0; i--)
+        {
+            while(!st.empty() and nums[i]>=nums[st.top()])
+            {
+                res = max(i - st.top(), res);
+                st.pop();
+            }
+        }
+        return res;
+        
     }
 };
