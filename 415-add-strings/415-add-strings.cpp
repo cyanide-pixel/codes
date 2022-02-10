d697053f5f6e15e1a6a8ed804754c54fcdb9c587
cyanide-pixel <54498203+cyanide-pixel@users.noreply.github.com>
2022-02-10 18:26:06 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (89.14%) - LeetHub

--- 415-add-strings/415-add-strings.cpp
+++ 415-add-strings/415-add-strings.cpp
@@ -1,22 +1,33 @@
 class Solution {
 public:
-    string addStrings(string n1, string n2) {
-         
+    string addStrings(string num1, string num2) {
         
-        if (n1.size() > n2.size()) swap(n1, n2);
-        
-        int l1 = n1.size(), l2 = n2.size(), sum = 0;
-        string res(l2 + 1, '0');
-        for (int i = l1 - 1, j = l2 - 1, k = 0; j >= 0;) {
-            sum = (i >= 0 ? n1[i--] - 48 : 0) + (n2[j--] - 48) + sum;
-            res[k++] = (sum > 9? sum - 10 : sum) + 48;
-            sum = sum > 9;
+        int carry = 0, a = 0, b = 0, val = 0, i = num1.size()-1 , j = num2.size()-1;
+        string sm = "";
+		while((i >= 0)||(j >= 0)){
+		   if(i>=0){
+                a = num1[i] - '0';
+            }
+            else{ 
+                a = 0;
+            }
+			if(j>=0){
+                 b = num2[j] - '0';    
+            }
+            else{
+                b = 0;
+            }
+			val = (a+b+carry)%10;
+            sm += (val+'0');
+            carry = (a+b+carry)/10;
+			
+            i--;
+            j--;
         }
-        
-        if (sum) res.back() = 49;
-        else res.pop_back();
-        
-        reverse(begin(res), end(res));
-        return res;
+        if(carry>0){
+            sm+=carry+'0';
+        }
+        reverse(sm.begin(), sm.end());
+    return sm;
     }
 };
