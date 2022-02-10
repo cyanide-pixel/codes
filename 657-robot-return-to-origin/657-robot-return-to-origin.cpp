c20f21ae8142513c1cbfc91bab4511c2c1cc42aa
cyanide-pixel <54498203+cyanide-pixel@users.noreply.github.com>
2022-02-10 08:32:03 +0530

    Time: 10 ms (79.44%), Space: 8 MB (47.57%) - LeetHub

---
+++ 657-robot-return-to-origin/657-robot-return-to-origin.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        
+        int a=0,b=0;
+        for(int i=0;i<moves.size();i++){
+            if(moves[i]=='U'){
+                a++;
+            }
+            else if(moves[i]=='D'){
+                a--;
+            }
+            else if(moves[i]=='R'){
+                b++;
+            }
+            else{
+                b--;
+            }
+        }
+        if(a==0 && b==0){
+            return true;
+        }
+        return false;
+    }
+};
