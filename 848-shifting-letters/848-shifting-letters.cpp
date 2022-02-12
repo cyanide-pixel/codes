3f96f697b3aa136549b14c7e765035b63154cdd7
cyanide-pixel <54498203+cyanide-pixel@users.noreply.github.com>
2022-02-12 14:30:05 +0530

    Time: 146 ms (64.35%), Space: 67.7 MB (81.12%) - LeetHub

--- 848-shifting-letters/848-shifting-letters.cpp
+++ 848-shifting-letters/848-shifting-letters.cpp
@@ -8,7 +8,7 @@ public:
         for(i=size-2; i>=0; i--)
             shifts[i] = (shifts[i]+shifts[i+1])%26;
         for(i=0; i<size; i++)
-           s[i] = (s[i]-'a'+shifts[i])%26 +'a';
+           s[i] = (s[i]-'a'+shifts[i])%26 + 'a';
 
         return s;
     }
