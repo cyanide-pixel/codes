9a306a822980e0b026a3b7ae167f29d25e7e369d
cyanide-pixel <54498203+cyanide-pixel@users.noreply.github.com>
2022-02-16 19:25:26 +0530

    Time: 0 ms (100.00%), Space: 6 MB (16.68%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,20 +1,29 @@
 class Solution {
 public:
+    void multiply(vector<vector<int>> &M ,vector<vector<int>> &A){
+        int M11 = M[0][0] * A[0][0] + M[0][1] * A[1][0];
+        int M12 = M[0][0] * A[0][1] + M[0][1] * A[1][1];
+        int M21 = M[1][0] * A[0][0] + M[1][1] * A[1][0];
+        int M22 = M[1][0] * A[0][1] + M[1][1] * A[1][1];
+        
+        M[0][0] = M11 , M[0][1] =M12 , M[1][0] =M21 , M[1][1] = M22;   
+    }
+    void power(vector<vector<int>> &M , int k){  
+        if(k==0 or k==1)
+            return ;
+        power(M , k/2);
+        multiply(M,M);
+        if(k&1){
+            vector<vector<int> > A {{1,1},{1,0}};
+            multiply(M, A);
+        }
+    }
+public:
     int fib(int n) {
-        int first = 0;
-        int second = 1;
-        int ans =0;
-        if(n==0){
-            return 0;
-        }else if (n==1){
-            return 1;
-        }else{
-        for(int i=1;i<n;i++){
-            ans = first + second;
-            first = second;
-            second = ans;
-        }
-        }
-        return ans;
+       if(n==0) return  0 ;
+        vector<vector<int> > M {{1,1},{1,0}};
+        power(M , n-1);
+        return M[0][0];
+        
     }
 };
