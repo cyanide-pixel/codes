cd06b13a275863c9399ad65f3a108ee294741e13
cyanide-pixel <54498203+cyanide-pixel@users.noreply.github.com>
2022-02-16 18:44:03 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (96.38%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int fib(int n) {
+        int first = 0;
+        int second = 1;
+        int ans =0;
+        if(n==0){
+            return 0;
+        }else if (n==1){
+            return 1;
+        }else{
+        for(int i=1;i<n;i++){
+            ans = first + second;
+            first = second;
+            second = ans;
+        }
+        }
+        return ans;
+    }
+};
