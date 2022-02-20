b924130c25dcdffec86a6afca38b87417d868436
cyanide-pixel <54498203+cyanide-pixel@users.noreply.github.com>
2022-02-20 15:10:21 +0530

    Time: 5 ms (10.99%), Space: 6 MB (26.74%) - LeetHub

--- 461-hamming-distance/461-hamming-distance.cpp
+++ 461-hamming-distance/461-hamming-distance.cpp
@@ -1,22 +1,12 @@
 class Solution {
 public:
     int hammingDistance(int x, int y) {
-        		x=x^y; 
-
-//         Then i used y as a counter to see how many bits are set
-		y=0;
-
-//         This loop will run until x become 0
+        	
+        x=x^y; 
+        y=0;
 		while(x){
 
-//             This if condition will check whether the 0th index is set or not
-			if(x&1){
-
-//             increasing the counter if the bit is set    
-				y++;    
-			}
-
-//             right shifting the bits of x by 1 
+			if(x&1) y++;
 			x=x>>1;
 		}
 		return y;
