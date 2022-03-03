128f636981f10faeaef33f723ef1001f32af6805
cyanide-pixel <54498203+cyanide-pixel@users.noreply.github.com>
2022-03-03 19:02:36 +0530

    Time: 53 ms (51.58%), Space: 73.8 MB (20.86%) - LeetHub

--- 445-add-two-numbers-ii/445-add-two-numbers-ii.cpp
+++ 445-add-two-numbers-ii/445-add-two-numbers-ii.cpp
@@ -16,14 +16,16 @@ public:
         while(list1){stack1.push(list1->val); list1 = list1->next;}
         while(list2){stack2.push(list2->val); list2 = list2->next;}
         int sum = 0;
+        int carry = 0;
         ListNode* currNode = nullptr;
         while(stack1.size() or stack2.size()){
             int val1 = stack1.size() ? stack1.top() : 0;
             int val2 = stack2.size() ? stack2.top() : 0;
             if(stack1.size()) stack1.pop();
             if(stack2.size()) stack2.pop();
-            sum = val1 + val2 + sum;
+            sum = val1 + val2 + carry;
             currNode = new ListNode(sum%10, currNode);
+            carry = sum/10;
             sum /= 10;
         }
         if(sum){currNode = new ListNode(sum, currNode);}
