cb18403cae35d35563ebce9de542589515a2bbd0
cyanide-pixel <54498203+cyanide-pixel@users.noreply.github.com>
2022-07-24 01:19:09 +0530

    Time: 174 ms (20.73%), Space: 16.7 MB (40.84%) - LeetHub

---
+++ 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minSteps(string s, string t) {
+        unordered_map<char, int> mp;
+        for(int i=0;i<s.length();i++)
+        {
+            mp[s[i]]++;
+        }
+        
+        int count=0;
+        for(int j=0;j<t.length();j++)
+        {
+            if(mp[t[j]] && mp[t[j]] >0)
+            {
+                mp[t[j]]--;
+            }
+            
+            else
+                count++;
+        }
+        
+        return count; 
+    }
+};
