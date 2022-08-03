3511cec85a84b93d7b0f7e2363b18fde641db212
cyanide-pixel <54498203+cyanide-pixel@users.noreply.github.com>
2022-08-03 15:38:37 +0530

    Time: 8 ms (27.88%), Space: 6 MB (25.28%) - LeetHub

---
+++ 69-sqrtx/69-sqrtx.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        int s = 0;
+        int e = x;
+        while(s<=e) {
+            long long mid = s+(e-s)/2;
+            if(mid*mid == x) {
+                return mid;
+            }
+            else if(mid*mid>x) {
+                e = mid-1;
+            }
+            else s =mid+1;
+        }
+        return e;
+    }
+};
