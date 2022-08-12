3e0a5539349e5c4c79e52b46f28ac45d87271d31
cyanide-pixel <54498203+cyanide-pixel@users.noreply.github.com>
2022-08-12 21:52:52 +0530

    Time: 38 ms (56.30%), Space: 25.2 MB (11.91%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/238-product-of-array-except-self.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> lp(n);
-        vector<int> rp(n); 
-        vector<int> ans(n);
-        
+        vector<int> lp(nums.size());
+        vector<int> rp(nums.size());
+        vector<int> ans(nums.size());
+        int n =nums.size();
         lp[0] = 1;
         rp[n-1] = 1;
-        for(int i=1; i<n; i++) {
+        
+        for(int i=1; i<nums.size(); i++) {
             lp[i] = nums[i-1]*lp[i-1];
         }
-        
-        for(int i = n-2; i>=0; i--) {
+        for(int i=nums.size()-2; i>=0; i--) {
             rp[i] = nums[i+1]*rp[i+1];
         }
         
         for(int i=0; i<n; i++) {
-            ans[i] = rp[i] *lp[i];
+            ans[i] = lp[i]* rp[i];
         }
-        
         return ans;
-        
     }
-}; 
+};
