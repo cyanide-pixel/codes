fcaffc60751c150893eebf375ec2831d153fcbfc
cyanide-pixel <54498203+cyanide-pixel@users.noreply.github.com>
2022-08-14 12:37:16 +0530

    Time: 8 ms, Memory: 20.4 MB - LeetHub

--- diameter-of-binary-tree/diameter-of-binary-tree.cpp
+++ diameter-of-binary-tree/diameter-of-binary-tree.cpp
@@ -11,20 +11,30 @@
  */
 class Solution {
 public:
-    int heightOfBinaryTree(TreeNode* root) {
+    pair<int, int> hd(TreeNode* root) {
         if(root == NULL) {
-            return 0;
+            pair<int, int> k;
+            k.first = 0;
+            k.second = 0;
+            return k;
         }
         
-        return 1+ max(heightOfBinaryTree(root->left), heightOfBinaryTree(root->right));
+        pair<int, int> leftAns = hd(root->left);
+        pair<int, int> rightAns = hd(root->right);
+        
+        int lh = leftAns.first;
+        int ld = leftAns.second;
+        int rh = rightAns.first;
+        int rd = rightAns.second;
+        
+        pair<int, int> overAll;
+        overAll.first = 1 + max(lh ,rh);
+        overAll.second = max(lh+rh, max(ld, rd));
+        return overAll;
+        
     }
     int diameterOfBinaryTree(TreeNode* root) {
-        if(root == NULL) return 0;
-        
-        int op1 = heightOfBinaryTree(root->left) + heightOfBinaryTree(root->right);
-        int op2 = diameterOfBinaryTree(root->left);
-        int op3 = diameterOfBinaryTree(root->right);
-        
-        return max(op1, max(op2, op3));
+        pair<int, int> p = hd(root);
+        return p.second;
     }
 };
