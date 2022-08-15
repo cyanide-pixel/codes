32fb64518f857926c0ef0ed086be5f17de35d8a2
cyanide-pixel <54498203+cyanide-pixel@users.noreply.github.com>
2022-08-15 13:52:47 +0530

    Time: 86 ms (68.27%), Space: 55.5 MB (15.08%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
@@ -3,15 +3,19 @@ public:
     int longestOnes(vector<int>& nums, int k) {
         int ans = 0;
         
-        for(int i=0, j=0; i<nums.size(); i++) {
-            if(nums[i]==0) k--;
+        for(int i =0, j=0 ;i<nums.size(); i++) {
+            if(nums[i] == 0) k--;
+            
             while(k<0) {
-                if(nums[j] == 0) k++;
+                if(nums[j] == 0) {
+                    k++;
+                } 
                 j++;
+                
             }
             
-            int pop = i-j+1;
-            ans = max(ans, pop);
+            int len = i-j+1;
+            ans = max(ans, len);
         }
         return ans;
     }
