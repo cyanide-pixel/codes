412e42ce818239a96922d0178026729d8b21273f
cyanide-pixel <54498203+cyanide-pixel@users.noreply.github.com>
2022-08-15 16:19:29 +0530

    Time: 4 ms (36.13%), Space: 7.7 MB (76.61%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,20 +1,26 @@
 class Solution {
 public:
-    int helper(vector<int>& nums, vector<int>&dp, int idx) {
-        if(idx == 0) return nums[idx];
-        if(idx<0) return 0;
-        if(dp[idx] != -1) return dp[idx];
+    int helper(vector<int>& nums) {
+        int prev = nums[0];
+        int prev2 = 0;
+        int curMax;
+        for(int i=1; i<nums.size(); i++) {
+            int pick = nums[i];
+            if(i>1) pick+=prev2;
+            int notPick = 0+prev;
+            
+            curMax = max(pick, notPick);
+            prev2= prev;
+            prev = curMax;
+        }
         
-        int pick = nums[idx]+helper(nums, dp, idx-2);
-        int nonPick = 0 + helper(nums, dp, idx-1);
-        
-        return dp[idx] = max(pick, nonPick);
+        return curMax;
     }
     int rob(vector<int>& nums) {
-        vector<int>dp(nums.size(), -1);
-        int k = helper(nums, dp,  nums.size()-1);
         
         
+        if(nums.size() == 1) return nums[0];
+        int k = helper(nums);
         return k ;
     }
 };
