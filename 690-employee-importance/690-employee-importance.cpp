0d6e7437fb521f4f1330a89f672a7bc02d61c28e
cyanide-pixel <54498203+cyanide-pixel@users.noreply.github.com>
2022-08-21 20:47:53 +0530

    Time: 46 ms (64.48%), Space: 24.5 MB (16.30%) - LeetHub

---
+++ 690-employee-importance/690-employee-importance.cpp
@@ -0,0 +1,32 @@
+/*
+// Definition for Employee.
+class Employee {
+public:
+    int id;
+    int importance;
+    vector<int> subordinates;
+};
+*/
+
+class Solution {
+public:
+    int dfs(vector<Employee*> emp, int id, vector<bool> &vis) {
+        int tot_sum=0;
+        for(auto m:emp) {
+            if(id == m->id) 
+            {
+                tot_sum+=m->importance;
+                for(auto k:m->subordinates) 
+                {
+                    tot_sum+= dfs(emp, k, vis);
+                }
+            }
+        }
+        return tot_sum;
+    }
+    int getImportance(vector<Employee*> employees, int id) {
+        vector<bool> vis(2001, false);
+        int sum = dfs(employees, id, vis);
+        return sum;
+    }
+};
