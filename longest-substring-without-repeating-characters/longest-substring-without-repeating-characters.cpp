31507cfa1593a33dc0fa231859a41adab5890df5
cyanide-pixel <54498203+cyanide-pixel@users.noreply.github.com>
2022-08-26 08:41:43 +0530

    Time: 23 ms, Memory: 8.4 MB - LeetHub

--- longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        unordered_map<char, int> chars;
-
+        unordered_map<char, int> mp;
+        
         int left = 0;
         int right = 0;
-
         int res = 0;
-        while (right < s.length()) {
+        
+        while(right<s.length()) {
             char r = s[right];
-            chars[r]++;
-
-            while (chars[r] > 1) {
+            mp[r]++;
+            
+            while(mp[r]>1) {
                 char l = s[left];
-                chars[l]--;
+                mp[l]--;
                 left++;
             }
-
-            res = max(res, right - left + 1);
-
+            
+            res = max(res, right-left+1);
             right++;
         }
-
         return res;
+        
     }
 };
